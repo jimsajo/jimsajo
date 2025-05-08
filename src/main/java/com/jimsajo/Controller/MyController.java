b092003eda62ac927a01c3b1ce44fa9ee96a632a
KJH <janghwan4524@nvaer.com>
2025-05-08 10:48:47 +0900

    2025-05-08

---
+++ src/main/java/com/jimsajo/Controller/MyController.java
@@ -0,0 +1,13 @@
+package com.jimsajo.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MyController {
+	@RequestMapping("/")
+	public String root() {
+		return "test";
+	}
+	
+}
